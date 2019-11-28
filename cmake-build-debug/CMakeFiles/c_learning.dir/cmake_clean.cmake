7ad4fb2006fdae714a8b1d2a85bafc63e753e191
liusheng <silenceon14@gmail.com>
2019-11-28 15:25:01 +0800

    initial

---
+++ cmake-build-debug/CMakeFiles/c_learning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_learning.dir/main.cpp.o"
+  "c_learning.exe"
+  "c_learning.pdb"
+  "libc_learning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i=10;i<20; i++)
+    {
+        cout << "ok"<<endl;
+    }
+    return 0;
+}
